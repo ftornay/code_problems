1e117b1901caffa05b85899d7300619974402740
Francisco Tornay <echon.noun@gmail.com>
2020-11-25 15:52:18 +0100

    Now URLify stores only number of spaces, not their positions

--- URLify.py
+++ URLify.py
@@ -18,26 +18,27 @@ def str2l(s):
 def URLify(url_s, len_s):
     """ We change the string in_place to save space """
     # Find the spaces
-    spaces = []
-    for i, c in enumerate(url_s):
+    n_spaces = 0
+    for c in url_s:
         if c == ' ':
-            spaces.append(i)
+            n_spaces += 1
     # We need to know to which place to copy
     end_copy = len_s
     # Positions we need to skip
-    displacement = len(spaces) * 2
+    displacement = n_spaces * 2
     # We move back from space to space
-    for i in range(len(spaces), 0, -1):
-        pos_space = spaces[i-1]
-        copy_to = pos_space + displacement + 1
-        copy_to_end = copy_to + end_copy - pos_space - 1
-        # Copy from space position to last space or end of string
-        url_s[copy_to:copy_to_end] = url_s[pos_space+1:end_copy]
-        url_s[copy_to-3] = '%'
-        url_s[copy_to-2] = '2'
-        url_s[copy_to-1] = '0'
-        displacement -= 2
-        end_copy = pos_space
+    for i in range(len(url_s), 0, -1):
+        if url_s[i-1] == ' ':
+            pos_space = i-1
+            copy_to = pos_space + displacement + 1
+            copy_to_end = copy_to + end_copy - pos_space - 1
+            # Copy from space position to last space or end of string
+            url_s[copy_to:copy_to_end] = url_s[pos_space+1:end_copy]
+            url_s[copy_to-3] = '%'
+            url_s[copy_to-2] = '2'
+            url_s[copy_to-1] = '0'
+            displacement -= 2
+            end_copy = pos_space
 
 if __name__ == '__main__':
     s1 = 'Hola adiós'
